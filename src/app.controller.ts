4b57a3120118c6b01d58332deccaa0213eeb5176
senuravihanjayadeva <senurajayadeva@gmail.com>
2023-01-19 08:41:49 +0530

    Implement protected route and JWT strategy guards

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
@@ -12,6 +12,12 @@ export class AppController {
   @Post('auth/login')
   async login(@Request() req:any) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req:any) {
+    return req.user;
   }
 
   @Get()
