3018f34da61f37e1a584f7672e41928a4e3b73f3
senuravihanjayadeva <senurajayadeva@gmail.com>
2023-01-19 08:33:36 +0530

    Authentication using passport-local

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req:any) {
+    return this.authService.login(req.user);
+  }
 
   @Get()
   getHello(): string {
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://NestUser:senu8864@cluster0.0uinm.gcp.mongodb.net/nestjsdb?retryWrites=true&w=majority'), ProductModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://NestUser:senu8864@cluster0.0uinm.gcp.mongodb.net/nestjsdb?retryWrites=true&w=majority'), ProductModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
